680a6ea0b138ee3310bdcda3caec5b93e9916c8d
odewumiolumide <96873080+odewumiolumide@users.noreply.github.com>
2023-03-29 16:28:55 -0300

    pot
    pot

---
+++ tio.js
@@ -0,0 +1,12 @@
+const menu = document.querySelector('#mobile-menu');
+const menuLinks = document.querySelector('.nav-menu');
+const formButton = document.querySelector('.form-button'); 
+menu.addEventListener('click',  function(){
+    menu.classList.toggle('is-active');
+    menuLinks.classList.toggle('active');
+});
+
+
+document.querySelector(".form-button").addEventListener("click" , function(){
+  formButton.innerHTML = "check your email for confirmation";
+});
